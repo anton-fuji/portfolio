95752de5c843bb4a18d2fcb89d33ed6de9a5a500
Fuji <fujiji.entire1414@gmail.com>
2025-04-28 17:49:24 +0900

    setup TS + Vite

---
+++ portfolio/tailwindcss.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{ts,tsx}"], // v4からこの書き方
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}  
