873ae6b9aa68f9c06527c6b97288edadeda3a3f5
Fuji <fujiji.entire1414@gmail.com>
2025-04-28 19:22:17 +0900

    modify contents

---
+++ portfolio/src/mydata/data.ts
@@ -0,0 +1,43 @@
+// import { Project, Skill, SocialLink } from "../types";
+import {SocialLink} from "../types";
+
+// export const SKILLS: Skill[] = [
+//     { name: "JavaScript", level: 5, category: "languages" },
+//     { name: "TypeScript", level: 4, category: "languages" },
+//     { name: "React", level: 5, category: "frontend" },
+//     { name: "Node.js", level: 4, category: "backend" },
+//     { name: "HTML/CSS", level: 5, category: "frontend" },
+//     { name: "TailwindCSS", level: 4, category: "frontend" },
+// ];
+
+export const SOCIAL_LINKS: SocialLink[] = [
+    {
+        name: "GitHub",
+        url: "https://github.com/anton-fuji",
+        icon: "Github",
+    },
+    {
+        name: "X",
+        url: "",
+        icon: "X",
+    },
+    {
+        name: "Zenn",
+        url: "https://zenn.dev/fuuji",
+        icon: "Zenn",
+    },
+    {
+        name: "Qiita",
+        url: "https://qiita.com/fujifuji1414",
+        icon: "Qiita",
+    },
+];
+
+export const PERSONAL_INFO = {
+    name: "Kazuki Fujimoto",
+    title: "",
+    description: "やっほー",
+    location: "Okayama, Japan",
+    email: "test@example.com",
+    profileImage: ""
+};
