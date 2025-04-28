fe417beced99b08cabfe26fce0604ef2eb504cca
Fuji <fujiji.entire1414@gmail.com>
2025-04-28 21:52:37 +0900

    fix

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 import './App.css'
 import Header from './layouts/Header'
 import {PERSONAL_INFO} from './mydata/data'
-import Layout from './layouts';
+
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className='relative'>
       {/* <Header /> */}
-      <Layout
+      
     </div>
   )
 }
