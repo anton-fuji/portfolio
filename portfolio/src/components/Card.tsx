ef20dadb64365747b683ad075d84995fde45b69a
Fuji <fujiji.entire1414@gmail.com>
2025-05-03 15:50:41 +0900

    カードコンポーネント用のtwMerge

---
+++ portfolio/src/components/Card.tsx
@@ -0,0 +1,22 @@
+import type { FC, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge'
+
+export type CardProps = {
+    children: ReactNode;
+    className?: string;
+};
+
+const Card: FC<CardProps> = ({ children, className }) =>{
+    return (
+        <div
+            className={twMerge(
+                "rounded-lg border border-gray-700 bg-[#0a0a0a] pt-4",
+                className
+            )}
+            >
+                {children}
+        </div>
+    )
+}
+
+export default Card;
