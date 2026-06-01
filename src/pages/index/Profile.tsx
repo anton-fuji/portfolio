e0c4e37b9697100116624b9ad04703d64cecd7d7
Fuji <fujiji.entire1414@gmail.com>
2026-06-01 22:12:29 +0900

    fix pages

--- src/pages/index/Profile.tsx
+++ src/pages/index/Profile.tsx
@@ -1,7 +1,9 @@
 import type { FC } from "react";
 import ProfileIcon from "../../assets/fuji-icon.jpg";
+import gopherGif from "../../assets/gopher.gif";
 import { PERSONAL_INFO } from "../../mydata/data";
 import Socials from "./Socials";
+import PixelTransition from "../../components/PixelTransition";
 
 const profile = {
   name: PERSONAL_INFO.name,
@@ -14,13 +16,28 @@ const Profile: FC = () => {
       <div className="relative mx-auto flex h-64 w-64 items-center justify-center sm:h-72 sm:w-72 lg:mx-0, lg:justify-self-">
         <div className="absolute inset-0 rounded-full border border-white/10 bg-white/[0.03] shadow-[0_0_70px_rgba(112,228,166,0.12)]" />
         <div className="absolute inset-5 rounded-full border border-dashed border-white/16" />
-        <div className="relative h-44 w-44 overflow-hidden rounded-full border border-white/25 shadow-2xl sm:h-52 sm:w-52">
-          <img
-            className="h-full w-full object-cover"
-            src={ProfileIcon}
-            alt=""
-          />
-        </div>
+        <PixelTransition
+          gridSize={9}
+          pixelColor="#07151d"
+          animationStepDuration={0.45}
+          className="relative h-44 w-44 rounded-full border border-white/25 shadow-2xl sm:h-52 sm:w-52"
+          firstContent={
+            <img
+              className="h-full w-full object-cover"
+              src={ProfileIcon}
+              alt=""
+            />
+          }
+          secondContent={
+          <div className="flex h-full w-full items-center justify-center bg-[#07151d]">
+            <img
+              className="h-[58%] w-[58%] object-contain [image-rendering:pixelated]"
+              src={gopherGif}
+              alt=""
+            />
+          </div>
+          }
+        />
       </div>
 
       <div className="flex flex-col gap-5 text-center text-white lg:text-left">
