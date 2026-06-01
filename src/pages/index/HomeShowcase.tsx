ddce016e23dcb6c0164da0371bffb00c21b5dcb3
Fuji <fujiji.entire1414@gmail.com>
2026-06-01 21:20:07 +0900

    refactor

---
+++ src/pages/index/HomeShowcase.tsx
@@ -0,0 +1,13 @@
+import Profile from "./Profile";
+
+const HomeShowcase = () => {
+  return (
+    <main className="relative mx-auto flex w-full max-w-6xl flex-col gap-24 py-20">
+      <Profile />
+
+      {/* Featured work / Writing / Workbench ... */}
+    </main>
+  );
+};
+
+export default HomeShowcase;
